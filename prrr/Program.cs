e20ed6bb9d855bb5e30756a2ceee00e4aa33b155
Евгений Радченко <evgeni.radchenko2000@gmail.com>
2020-11-20 18:58:28 +0300

    Добавьте файлы проекта.

---
+++ prrr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double y = 0;
+            while (y <= 15)
+            {
+                double y1 = x + y;
+                y = y1;
+                Console.WriteLine("x = " + x);
+                Console.WriteLine("y1 = " + y1);
+                x += 0.2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
